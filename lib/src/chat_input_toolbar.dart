ee5675ae8ec6ef46fbf5251c73804917a13150f8
bit-burger <65500763+bit-burger@users.noreply.github.com>
2021-12-11 16:59:49 +0100

    Update chat_input_toolbar.dart

--- lib/src/chat_input_toolbar.dart
+++ lib/src/chat_input_toolbar.dart
@@ -167,14 +167,6 @@ class ChatInputToolbar extends StatelessWidget {
       onTextChange!("");
 
       FocusScope.of(context).requestFocus(focusNode);
-
-      Timer(Duration(milliseconds: 150), () {
-        scrollController!.animateTo(
-          reverse ? 0.0 : scrollController!.position.maxScrollExtent + 30.0,
-          curve: Curves.easeOut,
-          duration: const Duration(milliseconds: 300),
-        );
-      });
     }
   }
 }
